f746e82f27bed314b6271c55bec87b00ae11e5b6
ZakaDev22 <elfakharzakaria22@gmail.com>
2025-01-17 22:39:26 +0000

    Add The Project Installer Logic And Service Dependacies First Service Tests Has Ben Feiled Update Connection String With MyUserName And Password On SQL Server Update File Path For More Readability  Reinstallation The service Couple times Service Result Befor : Fieled - Fieled - Fieled ❌

--- ProjectInstaller.cs
+++ ProjectInstaller.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ServiceProcess;
 
 namespace MyDatabaseBackupService
 {
@@ -8,6 +9,33 @@ namespace MyDatabaseBackupService
         public ProjectInstaller()
         {
             InitializeComponent();
+
+            // Service Process Installer
+            var serviceProcessInstaller = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem // Use LocalSystem or a specific user account with permissions
+            };
+
+            // Service Installer
+            var serviceInstaller = new ServiceInstaller
+            {
+                ServiceName = "MyBackupService",
+                DisplayName = "My Backup Service",
+                Description = "A service that creates database backups on a schedule.",
+                StartType = ServiceStartMode.Automatic // Automatically start the service on boot
+            };
+
+            // Set Dependencies
+            serviceInstaller.ServicesDependedOn = new string[]
+            {
+            "MSSQLSERVER",    // SQL Server 
+            "Schedule",       // Task Scheduler service
+            "eventlog"        // Event Log service
+            };
+
+            // Add installers to collection
+            Installers.Add(serviceProcessInstaller);
+            Installers.Add(serviceInstaller);
         }
     }
 }
